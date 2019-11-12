edd20340fd8f3d885faf67d7babb6db6e77a1bd9
dpayne <darby.payne@gmail.com>
2019-11-11 23:16:39 -0800

    Updating readme

--- cmake/DefineOptions.cmake
+++ cmake/DefineOptions.cmake
@@ -1,4 +1,4 @@
-option(BUILD_TESTS "Comple and run tests" ON)
+option(BUILD_TESTS "Comple and run tests" OFF)
 option(BUILD_PERF_TESTS "Compile and run performance tests" OFF)
 option(RUN_FORMATTER "Format codebase" OFF)
 option(GENERATE_DOCS "Build documentation" OFF)
@@ -7,4 +7,4 @@ option(RUN_CLANG_TIDY "Run clang tidy on codebase" OFF)
 # create compile_commands.json
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-set(default_build_type "Debug")
+set(default_build_type "Release")
